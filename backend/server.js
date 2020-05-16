cf8fc6c504bf63e30a8b357df859fd97160b7238
Jakob Udovic <ju8168@student.uni-lj.si>
2020-05-16 20:08:55 +0200

    fixed server.js, template for mongo, when I set it up, eventually

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,30 @@
 import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
-app.get('/', (req, res) => res.send("Hello world!"));
+const router = express.Router();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// mongoose.connect('mongodb://localhost:27017/issues');
+/*
+mongoose.connect('');
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('MongoDB database connection established successfully!');
+});
+*/
+
+// app.get('/', (req, res) => res.send("Hello world!"));
+app.use('/', router);
+
 var port = 4000;
-app.listen(port, () => console.log('Express server is running on port', port));
+app.listen(port, () => console.log('Express server is running on port', port, '\n'));
 
 
 /*
