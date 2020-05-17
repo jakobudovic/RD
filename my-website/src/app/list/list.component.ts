4b659a636fc8f0a677fe90af7b0806355ddfef96
Jakob Udovic <ju8168@student.uni-lj.si>
2020-05-17 11:49:05 +0200

    work on frontend

--- my-website/src/app/list/list.component.ts
+++ my-website/src/app/list/list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Task } from '../task.model';
+import { TaskService } from '../task.service';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  tasks: Task[];
 
-  constructor() { }
+  // injecting services
+  constructor(private taskService: TaskService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.taskService.getTasks().subscribe((tasks) => {
+      this.tasks = tasks;
+      console.log('tasks:');
+      console.log(tasks);
+    });
   }
 
+  // getting the tasks method
+  fetchTasks() {}
 }
---
+++ my-website/src/app/task.model.ts
@@ -0,0 +1,8 @@
+// Architecture/ class of Task model
+export interface Task {
+  id: String;
+  title: String;
+  description: String;
+  date: Date;
+  important: Boolean;
+}
